0b10e6e04bb9316e687a16b8792ffa33ba3b499c
Yaroslav Yermilov <yermilov17@gmail.com>
2017-01-25 02:07:48 +0200

    try to fix issue with request

--- articles_fetch_web_server.py
+++ articles_fetch_web_server.py
@@ -20,11 +20,8 @@ def fetch(user_id):
         response = json.loads(response_json)
         return json.dumps(fetch_pocket_links(response.accessToken))
     except URLError, e:
-        if hasattr(e, 'reason'):
-            return 'Something went wrong:', e.reason
-        if hasattr(e, 'code'):
-            return 'Something went wrong:', e.code
+        return 'Something went wrong:', e
 
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000)
