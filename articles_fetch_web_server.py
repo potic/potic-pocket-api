365cebe3bd62442deaf1da0b3e427128d606982a
Yaroslav Yermilov <yermilov17@gmail.com>
2017-04-22 15:52:29 +0300

    add parameter defaults

--- articles_fetch_web_server.py
+++ articles_fetch_web_server.py
@@ -14,9 +14,26 @@ def fetch_cached():
 @app.route('/fetch/<user_id>')
 def fetch(user_id):
     detailType = request.args.get('detailType')
+    if not detailType:
+        detailType = 'complete'
+
     count = request.args.get('count')
+    if not count:
+        count = 10
+    else:
+        count = int(count)
+
     offset = request.args.get('offset')
+    if not offset:
+        offset = 0
+    else:
+        offset = int(offset)
+
     since = request.args.get('since')
+    if not since:
+        since = 0
+    else:
+        since = int(since)
 
     userResponse = requests.get('http://pocket-square-users:8080/user/' + user_id).json()
     return json.dumps(fetch_pocket_links(userResponse["accessToken"], detailType, count, offset, since))
