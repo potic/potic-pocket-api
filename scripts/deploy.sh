bed3ede825e49c4f2d15c75f43925aa9d1b2a4b9
Yaroslav Yermilov <yermilov17@gmail.com>
2018-03-08 17:06:54 +0200

    restart container on failure

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -21,7 +21,7 @@ warn "Pulling latest docker image..."
 docker pull potic/potic-pocket-api:$TAG_TO_DEPLOY
 
 warn "Starting docker image..."
-docker run -dit --name potic-pocket-api -e POCKET_APP_KEY=$POCKET_APP_KEY -e ENVIRONMENT_NAME=$ENVIRONMENT_NAME -p 40403:5000 potic/potic-pocket-api:$TAG_TO_DEPLOY
+docker run -dit --name potic-pocket-api --restart on-failure -e POCKET_APP_KEY=$POCKET_APP_KEY -e ENVIRONMENT_NAME=$ENVIRONMENT_NAME -p 40403:5000 potic/potic-pocket-api:$TAG_TO_DEPLOY
 
 warn "Currently running docker images"
 docker ps -a
